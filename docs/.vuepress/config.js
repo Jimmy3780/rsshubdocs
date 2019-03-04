0edd6bf66319db1988f212d670146239df948a87
Jimmy0w0 <jimmy@jimmyowo.club>
2019-03-04 13:02:50 +0800

    commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'RSSHub第三方文档',
+    description: '万物皆可RSS',
+    head:[
+        ['link', { rel: 'icon', href: 'https://i.imgur.com/NZpRScX.png' }],
+    ],
+
+    themeConfig:{
+        nav: [
+            { text: '关于', link:'/content/about/' },
+            { text: '部署', link:'/content/install/' },
+            { text: '指南', link:'/content/api/' }
+        ]
+    }
+
+
+}
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+
+heroImage: https://ws1.sinaimg.cn/large/006m0LdAly1g0ovdauc6jj30d20d2wgm.jpg
+
+features:
+- title: 快速
+  details: 快速订阅和获得你想要的信息
+- title: 开源
+  details: 基于MIT许可证
+- title: 方便
+  details: 无论是订阅还是开发，都可以很快速的通过RSSHub建立自己想要的RSS源
